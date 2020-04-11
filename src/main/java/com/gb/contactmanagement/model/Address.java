9a9074551684301d79d93b12ef363b133fecaff8
Balasubramanian G <balasubramaniang@balasubramanians-macbook-pro.local>
2020-04-11 19:16:17 +0530

    Added Address and Phone

---
+++ src/main/java/com/gb/contactmanagement/model/Address.java
@@ -0,0 +1,24 @@
+package com.gb.contactmanagement.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Data
+@NoArgsConstructor
+public class Address {
+    @Field
+    private String addressLine1;
+    @Field
+    private String addressline2;
+    @Field
+    private String addressLine3;
+    @Field
+    private String city;
+    @Field
+    private String state;
+    @Field
+    private String country;
+    @Field
+    private String zipCode;
+}
---
+++ src/main/java/com/gb/contactmanagement/model/Phone.java
@@ -0,0 +1,16 @@
+package com.gb.contactmanagement.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Data
+@NoArgsConstructor
+public class Phone {
+    @Field("mobile")
+    private String mobile;
+    @Field("secondaryMobile")
+    private String secondaryMobile;
+    @Field("emergencyContactNumber")
+    private String emergencyNumber;
+}
