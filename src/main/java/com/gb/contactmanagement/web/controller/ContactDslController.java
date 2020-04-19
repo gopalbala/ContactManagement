4c12fbd1e526666eea5b5864ae1f7dda3fb550b5
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-04-19 22:30:37 +0530

    Added controller to find using dsl

---
+++ src/main/java/com/gb/contactmanagement/web/controller/ContactDslController.java
@@ -0,0 +1,31 @@
+package com.gb.contactmanagement.web.controller;
+
+import com.gb.contactmanagement.service.ContactQueryDSLService;
+import com.gb.contactmanagement.web.dto.ContactDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/contactservice/v1/dsl")
+public class ContactDslController {
+    @Autowired
+    ContactQueryDSLService contactQueryDSLService;
+
+    @RequestMapping(value = "/contact/firstName/{firstName}",
+            method = RequestMethod.GET, produces = {"application/JSON"})
+    public ResponseEntity<?> findByFirstName(@PathVariable String firstName) {
+        List<ContactDto> contactDtos = null;
+        contactDtos = contactQueryDSLService.findByFirstName(firstName);
+        if (contactDtos == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(contactDtos, HttpStatus.OK);
+    }
+
+}
