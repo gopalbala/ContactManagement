974feffe1feea22cf73b2329f1cc12a6a8dc1d20
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-05-10 16:06:12 +0530

    dbconfig updates

--- src/main/java/com/gb/contactmanagement/config/DbConfig.java
+++ src/main/java/com/gb/contactmanagement/config/DbConfig.java
@@ -1,29 +1,77 @@
 package com.gb.contactmanagement.config;
 
-import com.mongodb.*;
-import com.mongodb.client.MongoClients;
+import com.gb.contactmanagement.model.Contact;
+import com.gb.contactmanagement.service.ContactChangeStreams;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDbFactory;
+import org.springframework.data.mongodb.core.convert.MongoConverter;
+import org.springframework.data.mongodb.core.messaging.ChangeStreamRequest;
+import org.springframework.data.mongodb.core.messaging.DefaultMessageListenerContainer;
+import org.springframework.data.mongodb.core.messaging.MessageListenerContainer;
 
+@Slf4j
 @Configuration
 public class DbConfig {
     @Value("${spring.data.mongodb.host}")
     private String hostName;
     @Value("${spring.data.mongodb.database}")
     private String database;
+    @Autowired
+    ContactChangeStreams contactChangeStreams;
 
     //public @Bean
-    MongoTemplate mongoTemplate() {
-        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
-                .applyConnectionString(new ConnectionString(hostName))
-                .writeConcern(WriteConcern.JOURNALED)
-                .retryWrites(true)
-                .readConcern(ReadConcern.AVAILABLE)
-                .readPreference(ReadPreference.secondaryPreferred())
+//    MongoTemplate mongoTemplate() {
+//        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+//                .applyConnectionString(new ConnectionString(hostName))
+//                .writeConcern(WriteConcern.JOURNALED)
+//                .retryWrites(true)
+//                .readConcern(ReadConcern.AVAILABLE)
+//                .readPreference(ReadPreference.secondaryPreferred())
+//                .build();
+//
+//        MongoTemplate mongoTemplate = new MongoTemplate(MongoClients.create(), database);
+//        return mongoTemplate;
+//    }
+    @Value("${spring.data.mongodb.uri}")
+    private String uri;
+    @Autowired
+    private MongoConverter mongoConverter;
+
+    @Bean
+    public MongoDbFactory mongoDbFactory() {
+        return new SimpleMongoClientDbFactory(uri);
+    }
+
+    public @Bean
+    MongoTemplate mongoTemplate() throws Exception {
+        MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory(), mongoConverter);
+
+        return mongoTemplate;
+    }
+
+    @Bean
+    MessageListenerContainer messageListenerContainer(MongoTemplate template) {
+
+        DefaultMessageListenerContainer messageListenerContainer =
+                new DefaultMessageListenerContainer(template);
+
+        ChangeStreamRequest<Contact> request = ChangeStreamRequest.builder()
+                .collection("contacts")
+//                .filter(newAggregation(match(where("operationType").is("insert"))))
+//                .filter()
+                .publishTo(contactChangeStreams)
                 .build();
 
-        MongoTemplate mongoTemplate = new MongoTemplate(MongoClients.create(), database);
-        return mongoTemplate;
+        messageListenerContainer.register(request, Contact.class);
+
+        messageListenerContainer.start();
+
+        return messageListenerContainer;
     }
 }
